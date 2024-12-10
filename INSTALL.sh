1d5344668d50f023ef8ac7cb7b35ad735d57fc29
David Lopez Jr. <42970536+DLopezJr@users.noreply.github.com>
2024-12-10 12:51:44 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -e
 #PURPOSE: Installer 
 
-cp    gitpush.sh "$TMP/gitpush";
-chmod 755        "$TMP/gitpush";
+if [ $(id -u) -eq 0 ]; then {
+	PREFIX="/usr/local/bin";
+	cp    gitpush.sh "$PREFIX/gitpush";
+	chmod 755        "$PREFIX/gitpush";
+} else {
+	XDG_BIN_HOME="$HOME/.local/bin";
+	mkdir -p         "$XDG_BIN_HOME";
+	cp    gitpush.sh "$XDG_BIN_HOME/gitpush";
+	chmod 755        "$XDG_BIN_HOME/gitpush";
+} fi
