39d5cd0dc0350bc93ad5422ff3e3bcf64b974618
David Lopez Jr. <42970536+DLopezJr@users.noreply.github.com>
2024-12-10 12:57:18 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,11 +2,11 @@
 #PURPOSE: Installer 
 
 if [ $(id -u) -eq 0 ]; then {
-	PREFIX="/usr/bin";
+	PREFIX="/usr/bin"; # mingw doesnt have /usr/local in $PATH
 	cp    gitpush.sh "$PREFIX/gitpush";
 	chmod 755        "$PREFIX/gitpush";
 } else {
-	XDG_BIN_HOME="$HOME/.local/bin";
+	XDG_BIN_HOME="$HOME/bin"; # mingw doesnt have $HOME/.local/bin in $PATH
 	mkdir -p         "$XDG_BIN_HOME";
 	cp    gitpush.sh "$XDG_BIN_HOME/gitpush";
 	chmod 755        "$XDG_BIN_HOME/gitpush";
